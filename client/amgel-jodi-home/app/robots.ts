642a362ebf9d6507f13de71534c85df58f71035c
virajAllen <viraj.kini@allen.in>
2026-03-26 00:03:55 +0530

    chore:initial commit

---
+++ client/amgel-jodi-home/app/robots.ts
@@ -0,0 +1,14 @@
+import { MetadataRoute } from 'next'
+
+export default function robots(): MetadataRoute.Robots {
+  const baseUrl = 'https://amgeljodi.com'
+
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+    },
+    sitemap: `${baseUrl}/sitemap.xml`,
+    host: baseUrl,
+  }
+}
